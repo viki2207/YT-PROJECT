d9743eb3f6ba1b16ba84a35993a580a6f58e337f
viki2207 <vidhishah2207@gmail.com>
2023-04-08 13:50:59 +1000

    Last database changes in local backend

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 const app = express();
+
 //Middlewares
 app.use(express.json());
 app.use(cors());
 app.use("/books", router); //localhost:5030/books
 mongoose
-  .connect(
-    "mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore?retryWrites=true&w=majority"
-  )
+  .connect("mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore")
   .then(() => console.log("Connected to database"))
   .then(() => {
     app.listen(5030);
